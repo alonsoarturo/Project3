eb45719edc9df321661fa9c126ba18dabaec8db3
Arturo Alonso <alonsoarturo@ou.edu>
2019-10-24 17:10:56 -0500

    Modified MesoEquivalent.java

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,5 +1,48 @@
+import java.time.LocalDateTime;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract
 {
+	private LocalDateTime dateTimeNow;
+	
+	
+	@Override
+	int getValueOfSecond() {
+		
+		dateTimeNow();
+		int second = dateTimeNow.getSecond();
+		
+		return second;
+	}
+
+	@Override
+	void dateTimeNow() {
+		
+		dateTimeNow = LocalDateTime.now();
+		
+	}
+
+	@Override
+	void sleepForFiveSec() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void dateTimeOfOtherCity() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void dateTimeDifferentZone() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void timeZoneHashMap() {
+		// TODO Auto-generated method stub
+		
+	}
    
 }
