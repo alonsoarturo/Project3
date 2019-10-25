077c0afe95a47f84bf0a4285af19cb86417b87ed
Arturo Alonso <alonsoarturo@ou.edu>
2019-10-25 09:29:58 -0500

    Modified DateTimeTwo.java

--- DateTimeTwo.java
+++ DateTimeTwo.java
@@ -1,8 +1,24 @@
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
 
 public class DateTimeTwo {
-
+	
+	private LocalDateTime currentDateTime = LocalDateTime.now();
+	
 	public void daysOfCurrentMonth() {
-		// TODO Auto-generated method stub
+		
+		DateTimeFormatter dayOfWeekFormat = DateTimeFormatter.ofPattern("EEEE");
+		
+		String tenthDay = dayOfWeekFormat.format(currentDateTime.withDayOfMonth(10)).toUpperCase();
+		String eighteenthDay = dayOfWeekFormat.format(currentDateTime.withDayOfMonth(18)).toUpperCase();;
+		
+		
+		
+		System.out.println("The tenth day of this month is " 
+		+ tenthDay + " and eighteenth is " + eighteenthDay);
 		
 	}
 
