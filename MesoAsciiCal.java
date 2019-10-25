5df560e377b95f02738a04553dd0f550e1ac5d8a
Arturo Alonso <alonsoarturo@ou.edu>
2019-10-25 16:51:22 -0500

    Finished AsciiCallAvg and started MesoEquivalent

--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -2,14 +2,30 @@
 public class MesoAsciiCal extends MesoAsciiAbstract 
 {
 
+	protected MesoStation stationID;
+	
 	public MesoAsciiCal(MesoStation mesoStation) {
-		// TODO Auto-generated constructor stub
+		
+		this.stationID = mesoStation;
+		
 	}
 
 	@Override
 	int calAverage() {
-		// TODO Auto-generated method stub
-		return 0;
+		int calAvg = 0;
+		double[] asciiVal = new double[4];
+		double asciiAvg = 0.0;
+		String stationName = stationID.getStID();
+		
+		for (int i = 0; i < 4; i++) {
+			asciiVal[i] = (double) stationName.charAt(i);
+		}
+		
+		asciiAvg = (asciiVal[0] + asciiVal[1] + asciiVal[2] + asciiVal[3]) / 4.0;
+		
+		calAvg = (int) Math.round(asciiAvg);
+		
+		return calAvg;
 	}
    
 }
--- MesoEquivalent.java
+++ MesoEquivalent.java
@@ -1,14 +1,50 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MesoEquivalent {
 
+	private HashMap<String, Integer> stationMap = new HashMap<String, Integer>();
+	private ArrayList<String> stationArray;
+	private String stationID;
+	
 	public MesoEquivalent(String stId) {
-		// TODO Auto-generated constructor stub
+		
+		this.stationID = stId;
+		
+		try {
+			read("Mesonet.txt");
+		} catch (IOException e) {
+			System.out.println("error printing from file");
+			e.printStackTrace();
+		}
+		
 	}
 
 	public HashMap<String, Integer> calAsciiEqual() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void read(String filename) throws IOException {
+		
+		 BufferedReader br = new BufferedReader(new FileReader(filename));
+		 
+		 String strg = "";
+	   
+		 strg = br.readLine();
+		
+		 while (strg != null) {
+			
+			stationArray.add(strg.trim());
+	    	
+	    	strg = br.readLine();
+	     }
+	     br.close();
 	}
 
 }
